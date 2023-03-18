b6925ec003df1a98f9fcbef9a1c11a03f2b6a2e0
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-18 16:30:35 +0530

    core java

--- core_java/A.java
+++ core_java/A.java
@@ -1,6 +1,7 @@
 package core_java;
 
 public class A {
+	int x = 10;
 	public static void main(String[] args) {
 		System.out.println("first class");
 		A a = new A();
@@ -10,5 +11,9 @@ public class A {
 		
 		A a2 = new A();
 		System.out.println(a2);
+		
+		System.out.println(a2.x);
+		System.out.println(a.x);
+		System.out.println(a1.x);
 	}
 }
