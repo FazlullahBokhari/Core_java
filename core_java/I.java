d24559b9948f2d2daef185599f1f5ed32d8ffc9d
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-19 17:57:45 +0530

    core java

---
+++ core_java/I.java
@@ -0,0 +1,15 @@
+package core_java;
+
+public class I {
+	I(){
+		System.out.println(100);
+	}
+	I(int x){
+		System.out.println(x);
+	}
+	public static void main(String[] args) {
+		I i1 = new I();
+		I i2 = new I();
+		I i3 = new I(100);
+	}
+}
