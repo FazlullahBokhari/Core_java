a334cd564e11b72dca7ab313cd609b6088cc07c5
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-19 17:04:00 +0530

    core java

--- core_java/H.java
+++ core_java/H.java
@@ -4,10 +4,18 @@ public class H {
 	public static void main(String[] args) {
 		H h = new H();
 		h.test();
+		h.test1(100,200,300,400,500);
 	}
 	public void test() {
 		System.out.println(100);
 		return;
+	}
+	public void test1(int... x) {
+		System.out.println(x[0]);
+		System.out.println(x[1]);
+		System.out.println(x[2]);
+		System.out.println(x[3]);
+		System.out.println(x[4]);
 	}
 
 }
