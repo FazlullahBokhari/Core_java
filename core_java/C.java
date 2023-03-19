90ceb8599b1f08fe8cf99865f896a1c97b10284a
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-19 11:48:01 +0530

    core java

---
+++ core_java/C.java
@@ -0,0 +1,15 @@
+package core_java;
+
+public class C {
+	static int x = 10;
+	
+	public static void main(String[] args) {
+		System.out.println(C.x);
+		C c = new C();
+		c.test();
+	}
+	public void test() {
+		System.out.println(C.x);
+	}
+
+}
