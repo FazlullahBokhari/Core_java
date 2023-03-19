0cd50601a9ef0216f57a4535abd794623f9bce94
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-19 08:16:00 +0530

    core java

--- core_java/B.java
+++ core_java/B.java
@@ -4,8 +4,12 @@ public class B {
 	public static void main(String[] args) {
 		B b = new B();
 		b.test();
+		B.test1();
 	}
 	public void test() {
 		System.out.println(1200);
+	}
+	public static void test1() {
+		System.out.println(1400);
 	}
 }
