c62e158b56c41641cbf337a593fdff485a5089bd
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-20 16:39:17 +0530

    core java

---
+++ core_java/DownCasting.java
@@ -0,0 +1,20 @@
+package core_java;
+
+public class DownCasting {
+	public static void main(String[] args) {
+		float f = 10.3f;
+		double d = f;
+		System.out.println(d);
+		
+		int i = 10;
+		
+		byte b = (byte)i;
+		
+		long l = 100;
+		int t = (int)l;
+		System.out.println(b);
+		System.out.println(t);
+		
+		 
+	}
+}
---
+++ core_java/UpCasting.java
@@ -0,0 +1,12 @@
+package core_java;
+
+public class UpCasting {
+	public static void main(String[] args) {
+		byte i = 30;
+		short j = i;
+		System.out.println(j);
+		int a = 100;
+		long l = a;
+		System.out.println(l);
+	}
+}
