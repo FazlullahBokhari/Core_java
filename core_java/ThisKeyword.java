ba3c7db7cf65e297f5bc169847d64fcc368a83d4
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-21 17:06:25 +0530

    core java

--- core_java/ThisKeyword.java
+++ core_java/ThisKeyword.java
@@ -5,14 +5,19 @@ public class ThisKeyword {
 	public static void main(String[] args) {
 		System.out.println("Inside Main Method");
 		ThisKeyword t = new ThisKeyword();
-		System.out.println(t);
+		System.out.println("Address of the class inside reference variable: "+t);
 		System.out.println(t.x);
 		t.test();
 	}
 	public void test() {
-		System.out.println("Inside Test method");
-		System.out.println(this);
+		System.out.println("Inside Test Method");
+		System.out.println("Address of class inside test method (this keyword) method: "+this);
 		System.out.println(this.x);
+		this.test1();
+	}
+	public void test1() {
+		System.out.println("Inside Test1 Method");
+		System.out.println("Hello");
 	}
 
 }
