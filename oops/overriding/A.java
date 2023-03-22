4b29088d593c1a12946554b702e9d2afc8913770
fazlullahb0 <fazlullahb0@gmail.com>
2023-03-22 22:47:41 +0530

    override

--- oops/overriding/A.java
+++ oops/overriding/A.java
@@ -1,10 +1,16 @@
 package oops.overriding;
 
 public class A {
-	public void test() {
-		System.out.println(1000);
+	public void test(int x) {
+		System.out.println(x);
 	}
 	public void test1() {
 		System.out.println(15000);
+	}
+	public int test2(int x) {
+		return x;
+	}
+	float test3(float f) {
+		return f;
 	}
 }
--- oops/overriding/B.java
+++ oops/overriding/B.java
@@ -3,13 +3,27 @@ package oops.overriding;
 public class B extends A {
 	
 	@Override
-	public void test() {
-		System.out.println(5000);
+	public void test(int z) {
+		System.out.println(z);
+	}
+	
+	@Override
+	public int test2(int y) {
+		return y;
+	}
+	@Override
+	public float test3(float f1) {
+		return f1;
 	}
 	public static void main(String[] args) {
 		B b = new B();
-		b.test();
+		b.test(789100);
 		b.test1();
+		int f = b.test2(8000);
+		System.out.println(f);
+		float x = b.test3(800.025f);
+		System.out.println(x);
+		
 	}
 	
 }
