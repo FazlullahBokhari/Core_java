f96d26fe99b77d3c36f4c1d97ccff2336ca12746
fazlullahb0 <fazlullahb0@gmail.com>
2023-04-13 07:37:40 +0530

    regx

--- app_java/regx/B.java
+++ app_java/regx/B.java
@@ -1,5 +1,14 @@
 package app_java.regx;
 
-public class B {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class B {
+	public static void main(String[] args) {
+		Pattern p = Pattern.compile("\\s");
+		Matcher m = p.matcher("a6 b# z 9 D");
+		while(m.find()) {
+			System.out.println(m.start()+"---"+m.group());
+		}
+	}
 }
