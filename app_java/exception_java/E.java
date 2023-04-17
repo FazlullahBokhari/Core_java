7624193d1eac772f2a3921deeac9ba6d66df62e0
fazlullahb0 <fazlullahb0@gmail.com>
2023-04-17 15:11:42 +0530

    finally

---
+++ app_java/exception_java/E.java
@@ -0,0 +1,15 @@
+package app_java.exception_java;
+
+public class E {
+
+	public static void main(String[] args) {
+		try {
+			int a = 100/0;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			System.out.println("Finally block");
+		}
+	}
+
+}
